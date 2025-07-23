7b64b531e21ac22bad776015e927b9f62e496f2b
yurasong <yura.song@ulb.be>
2025-07-23 10:10:59 +0200

    embedding

--- 01_scRNA-seq/03_pySCENIC/06_Embedding.R
+++ 01_scRNA-seq/03_pySCENIC/06_Embedding.R
@@ -38,3 +38,38 @@ aucs <- CreateAssayObject(counts = auc_mtx)
 seuset_auc[["AUC"]] <- aucs
 
 DefaultAssay(seuset_auc) <- "AUC"
+
+# Plotting AUC value
+
+umap_coord <- as.data.frame(seuset_auc@reductions$umap@cell.embeddings)
+seuset_auc_matrix <- merge.data.frame(umap_coord,t(as.data.frame(seuset_auc@assays$AUC@data)),by=0)
+row.names(seuset_auc_matrix) <- seuset_auc_matrix$Row.names
+seuset_auc_matrix <- seuset_auc_matrix[, -1]
+
+# Plotting
+
+myPalette <- viridis(n = 10, option = "C", direction = -1)
+sc <- scale_colour_gradientn(colours = rev(myPalette))
+
+umap_color_scaled <- function(x)
+{
+  regulon <- x
+  regulon_d <- seuset_auc_matrix[,regulon]
+  regulon_plot <- ggplot(data=seuset_auc_matrix) + geom_point(aes(x=umap_1,y=umap_2,colour=regulon_d), size=1)
+  regulon_plot <- regulon_plot + labs(colour=regulon)
+  regulon_plot <- regulon_plot + sc
+  regulon_plot <- regulon_plot + theme_bw() + 
+    theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), 
+            legend.position = "none",
+            axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) 
+  
+  regulon_plot
+}
+
+umap_color_scaled("Klf5")
